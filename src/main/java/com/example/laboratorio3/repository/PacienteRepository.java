91c4ec67ca26782e2850d591319eba3e3d2dd6ed
alex-segovia <a20213849@pucp.edu.pe>
2024-04-18 21:51:27 -0500

    Se listaron los pacientes, se hizo la edición de pacientes y se pueden derivar pacientes a otros oftalmólogos

--- src/main/java/com/example/laboratorio3/repository/PacienteRepository.java
+++ src/main/java/com/example/laboratorio3/repository/PacienteRepository.java
@@ -1,8 +1,11 @@
 package com.example.laboratorio3.repository;
 
 import com.example.laboratorio3.entity.Clinica;
+import com.example.laboratorio3.entity.Oftalmologo;
 import com.example.laboratorio3.entity.Paciente;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -21,4 +24,17 @@ public interface PacienteRepository extends JpaRepository<Paciente,Integer> {
 
     @Query(nativeQuery = true,value = "select * from paciente where oftalmologo_id = ?1 and fecha_cita>now()")
     List<Paciente> listarPacientesPorAtender(int id_oftalmologo);
+
+    @Query(nativeQuery = true,value = "select * from paciente where id=?1")
+    Paciente obtenerPacientePorId(int id);
+
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true,value = "update paciente set numero_habitacion=?1 where id=?2")
+    void actualizarPaciente(int numero_habitacion, int id);
+
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true,value = "update paciente set oftalmologo_id=?1 where id=?2")
+    void derivarPaciente(int nuevoOftalmologo, int id);
 }
